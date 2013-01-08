70d5bc466c6f8ae96161a9b64c6ca89757611327
Gregory McWhirter <greg@hallofkvasir.org>
2013-01-07 22:01:21 -0800

    Disabling histogram

--- src/stats.py
+++ src/stats.py
@@ -718,7 +718,7 @@ def format_stats(data, prefix=None, gphx_file=None):
 
         ret += "{0}Mean: {1:.4%}\n".format(prefix, series.mean())
         ret += "{0}StdDev: {1:.4%}\n".format(prefix, series.standard_deviation())
-        ret += "{0}Histogram: {1}\n".format(prefix, series.histogram(bins=10))
+        #ret += "{0}Histogram: {1}\n".format(prefix, series.histogram(bins=10))
 
         if gphx_file is not None:
             plot = series.plot_histogram(figsize=6)
